d7abb434bc2517b9271031b73dda68b1e3083223
Roberto Torrico <roberto.t@medicalwebexperts.com>
2018-04-29 17:48:57 -0300

    Build storybook

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,79 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import H1 from '../src/components/H1/H1';
+import { Container, Grid, Col, Row } from '../src/components/Grid';
+// Styles
+import '../node_modules/doslotos-grid/dist/doslotos-grid.css';
+// Custom styles
+const styles = {
+  background: '#f2f2f2',
+  border: '1px solid #CCC',
+  textAlign: 'center',
+}
 
-storiesOf('Example', module)
-  .add('with text', () => (
-    <H1 text="😀 😎 👍 💯" />
+storiesOf('Grid', module)
+  .add('Full Grid', () => (
+    <Container>
+      <Grid>
+        <Row>
+          <Col xs={4} sm={3} md={2} style={styles}>
+            <p><code>{'xs={4} sm={3} md={2}'}</code></p>
+          </Col>
+          <Col xs={8} sm={9} md={10} style={styles}>
+            <p><code>{'xs={8} sm={9} md={10}'}</code></p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={8} style={styles}>
+            <p><code>{'sm={6} md={3}'}</code></p>
+          </Col>
+          <Col sm={4} style={styles}>
+            <p><code>{'sm={6} md={3}'}</code></p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={6} sm={3} style={styles}>
+            <p><code>{'xs={4} sm={3}'}</code></p>
+          </Col>
+          <Col xs={6} sm={3} style={styles}>
+            <p><code>{'xs={4} sm={3}'}</code></p>
+          </Col>
+          <Col xs={6} sm={3} style={styles}>
+            <p><code>{'xs={4} sm={3}'}</code></p>
+          </Col>
+          <Col xs={6} sm={3} style={styles}>
+            <p><code>{'xs={4} sm={3}'}</code></p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={4} sm={2} style={styles}>
+            <p><code>{'xs={4} sm={2}'}</code></p>
+          </Col>
+          <Col xs={4} sm={2} style={styles}>
+            <p><code>{'xs={4} sm={2}'}</code></p>
+          </Col>
+          <Col xs={4} sm={2} style={styles}>
+            <p><code>{'xs={4} sm={2}'}</code></p>
+          </Col>
+          <Col xs={4} sm={2} style={styles}>
+            <p><code>{'xs={4} sm={2}'}</code></p>
+          </Col>
+          <Col xs={4} sm={2} style={styles}>
+            <p><code>{'xs={4} sm={2}'}</code></p>
+          </Col>
+          <Col xs={4} sm={2} style={styles}>
+            <p><code>{'xs={4} sm={2}'}</code></p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={12} style={styles}>
+            <p><code>{'sm={12}'}</code></p>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
   ))
